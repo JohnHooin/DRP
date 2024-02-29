f52ab83dd74bf239e69bae28686e1a83ba2aab1b
maris-git <vtbtuyen2222004@gmail.com>
2024-02-29 20:20:57 +0700

    init commit

---
+++ Web/Public/inc/footer.php
@@ -0,0 +1,34 @@
+<!-- Giới thiệu -->
+<div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5" style="background-color: #e1ebfa;">
+    <!-- Thông tin nhóm -->
+    <div class="text-black mb-3 mb-md-0">
+        <p style="font-weight: 700">NHÓM 3 - CT07N01</p>
+        <div class="mb-3 mb-md-0 me-3">
+            <p style="font-weight: 700">Học viện Kỹ thuật mật mã</p>
+        </div>
+    </div>
+    <!-- Thành viên -->
+    <div class=" d-flex flex-column flex-md-row text-center text-md-start justify-content-between text-black">
+        <div class="mb-3 mb-md-0 me-3">
+            <p style="font-weight: 700">Nhóm trưởng</p>
+            <p style="font-weight: 600">Trần Quang Diệu</p>
+        </div>
+        <div class="mb-3 mb-md-0 me-3">
+            <p>Võ Thị Bích Tuyền</p>
+            <p>Lê Thanh Yên</p>
+        </div>
+        <div class="mb-3 mb-md-0">
+            <p>Ngô Nguyễn Hạnh Phúc</p>
+            <p>Mạch Tiến Duy</p>
+        </div>
+
+    </div>
+
+    <!-- Email nhóm -->
+    <div>
+        <p>Liên hệ</p>
+        <a href="#!" class="text-black mb-3 mb-md-0">
+            <p>abc@gmail.com</p>
+        </a>
+    </div>
+</div>
---
+++ Web/Public/inc/header.php
@@ -0,0 +1,129 @@
+<!-- Navbar -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a2af703eed.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="vendors/font-awesome-4.7.0/css/font-awesome.min.css" />
+    <script src="./../../Public/js/preload.js"></script>
+    <link rel="stylesheet" href="./../../Public/css/preloader_styles.css">
+    <title>PaPals-Enjoy your meals</title>
+    <style>
+        .divider:after,
+        .divider:before {
+            content: "";
+            flex: 1;
+            height: 1px;
+            background: #eee;
+        }
+        .h-custom {
+            height: calc(100% - 73px);
+        }
+        @media (max-width: 450px) {
+            .h-custom {
+                height: 100%;
+            }
+        }
+        .show {
+        display: none;  
+        }
+        .dropdown-toggle:hover .show {
+            display: block !important;
+            position: absolute;
+            top: 36px;
+            right: 0px;
+        }
+        </style>
+</head>
+<body>
+  <!-- <div class="loader_container">
+    <span class="loader">Loading....</span>
+  </div> -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e1ebfa;">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button
+        data-mdb-collapse-init
+        class="navbar-toggler"
+        type="button"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+  
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/3274/3274099.png"
+            height="50"
+            alt="PaPals"
+            loading="lazy"
+          />
+          <h3>PaPals</h3>
+        </a>
+        <!-- Left links -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size:16px">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blogs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Post</a>
+          </li>
+        </ul>
+        <!-- Left links -->
+      </div>
+      <!-- Collapsible wrapper -->
+  
+      <!-- Right elements -->
+      <div class="d-flex align-items-center">
+        <!-- Notifications -->
+            <!-- <i class="fas fa-bell me-3" style="height: 30"></i> -->
+            <!-- <span class="badge rounded-pill badge-notification bg-danger">1</span> -->
+        <!-- Avatar -->
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/3033/3033143.png"
+              class="rounded-circle me-3"
+              height="30"
+              alt="Black and White Portrait of a Man"
+            />
+            <button>Logout</button>
+          </a>
+          <!-- <ul
+              id="dropdown-menu"
+              class="show dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdownMenuAvatar"
+          >
+            <li>
+              <a class="dropdown-item" href="#">My profile</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Settings</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Logout</a>
+            </li>
+          </ul> -->
+        </div>
+      </div>
+      <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+</body>
